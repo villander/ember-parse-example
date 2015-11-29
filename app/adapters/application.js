42feb1a8e85186c14a7dbd91bddcae4a8388823e
Michael Villander <nousvillander@gmail.com>
2015-11-29 20:04:39 -0200

    .gitignore is now working

--- app/adapters/application.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.RESTAdapter.extend({
-  host: 'https://api.parse.com/1/classes',
-  headers: {
-    "X-Parse-Application-Id": "fS61UUBy1Vlpf6296lFTget2U7oYlNku8K6iEgBi",
-    "X-Parse-REST-API-Key": "gx55PFoUhLjbR4uCllzpLhRPrt3pOED0BvjyZ2K3"
-  }
-});
--- app/models/conversation.js
+++
@@ -1,6 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  other   : DS.attr('string'),
-  me : DS.attr('string')
-});
